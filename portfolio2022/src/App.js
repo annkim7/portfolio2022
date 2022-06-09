cd7bcb61f597c58f31c46797e9d05072ef7efc10
annkim7@github.com <ilc7748@gmail.com>
2022-06-09 16:53:19 +0900

    header style

--- portfolio2022/src/App.js
+++ portfolio2022/src/App.js
@@ -5,13 +5,23 @@ import './style/style.scss';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </header> */}
-
-      <div class="test">
-        sass 테스트 용
+      <header>
+        <div className="title">
+          <h1 className="logo">Portfolio 2022<b>Ann Kim</b></h1>
+        </div>
+        <div className="content">
+          <nav>
+            <ul className="nav-list">
+              <li>Home</li>
+              <li>List</li>
+              <li>Github</li>
+              <li>Notion</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div className="wrap">
+        content
       </div>
     </div>
   );
