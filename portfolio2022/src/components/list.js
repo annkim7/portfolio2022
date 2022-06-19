e9043e4921373d62ea161759cf7f25bbc2800980
annkim7@github.com <ilc7748@gmail.com>
2022-06-19 22:39:01 +0900

    build and test on the server

--- portfolio2022/src/components/list.js
+++ portfolio2022/src/components/list.js
@@ -4,11 +4,10 @@ import {useState, useEffect} from 'react';
 
 function List() {
   
-// let a = useSelector((state) => { return state } )
 let [list, setList] = useState([]);
 
 useEffect(()=>{
-  axios.get('http://localhost:8080/list').then((결과) => {
+  axios.get('/list').then((결과) => {
        setList(결과.data)
        console.log(결과.data)
   }).catch(()=>{
@@ -24,22 +23,10 @@ useEffect(()=>{
         <div className="content">
           <div className="list-box">
             <ul>
-              {/* <li>Portfolio2021</li>
-              <li>React, slider, motion</li>
-              <li>리액트로 만든 2021 포트폴리오 사이트</li> */}
               <li>{list[0]?.title}</li>
             </ul>
           </div>
         </div>
-        {/* <span>{a.testA[0]?.title}</span> */}
-        {/* <button onClick={()=>{
-      axios.get('/list').then((결과)=>{
-        console.log(결과.data)
-      })
-      .catch(()=>{
-        console.log('실패함')
-      })
-    }}>버튼</button> */}
     </section>
   );
 
