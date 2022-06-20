5784ce22f75c45daff117026c539ad138a26ac27
annkim7@github.com <ilc7748@gmail.com>
2022-06-20 14:37:37 +0900

    db & react connected

--- portfolio2022/src/components/list.js
+++ portfolio2022/src/components/list.js
@@ -15,15 +15,29 @@ useEffect(()=>{
   })
 }, [])
 
-
-
   return (
     <section className="list">
         <h2 className="main-title">List of my portfolio</h2>
         <div className="content">
           <div className="list-box">
             <ul>
-              <li>{list[0]?.title}</li>
+              <li className="head">
+                <b>title</b>
+                <span>keyword</span>
+                <p>description</p>
+              </li>
+              {
+                list.map(function(a, i){
+                  return(
+                    <li onClick={()=>window.open(`${list[i].link}`,'_blank')} key={i}>
+                      <b>{list[i].title}</b>
+                      <span>{list[i].keyword}</span>
+                      <p>{list[i].description}</p>
+                    </li>
+                  )
+                })
+              }
+              
             </ul>
           </div>
         </div>
